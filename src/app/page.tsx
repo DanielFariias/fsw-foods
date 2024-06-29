3e8275a152ffa4fcfe462eba4f89f38da18544cb
DanielFariias <danielfariias15@gmail.com>
2024-06-29 12:27:43 -0300

    feat: add initial search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Header } from '@/components/header'
+import { Header } from "@/components/header";
+import { Search } from "@/components/search";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </main>
-  )
+  );
 }
