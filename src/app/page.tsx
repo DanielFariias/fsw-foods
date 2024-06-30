ad3ead79934f587821c6d00e55107d819288a6fd
DanielFariias <danielfariias15@gmail.com>
2024-06-29 22:54:59 -0300

    feat: add promo banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { CategoryList } from '@/components/category-list'
 import { Header } from '@/components/header'
 import { Search } from '@/components/search'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="">
+        <Image
+          src="/promo-banner.png"
+          alt="hero"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain px-5 pt-6"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </main>
   )
