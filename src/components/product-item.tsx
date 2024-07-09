209b3a1b7a1fdf3fb4b953616ab492eee1eea723
DanielFariias <danielfariias15@gmail.com>
2024-07-08 23:50:15 -0300

    style: add border to product item

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -21,7 +21,7 @@ export function ProductItem({ product }: IProductItemProps) {
     <Link href={`/product/${product.id}`} className="min-w-[150px] space-y-2">
       <div className="relative h-[150px] w-full">
         <Image
-          className="rounded-lg object-cover shadow-md"
+          className="rounded-lg border border-muted object-cover shadow-md"
           src={product.imageUrl}
           alt={product.name}
           fill
